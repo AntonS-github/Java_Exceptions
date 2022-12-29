a84d7e658b571f233449c8514c70770ad3034dea
antons <surkov.ved@gmail.com>
2022-12-29 16:57:17 +0300

    HW1 Exceptions done

--- Java projects/Exceptions/src/Exceptions_HW1.java
+++ Java projects/Exceptions/src/Exceptions_HW1.java
@@ -3,7 +3,6 @@ import java.util.logging.Logger;
 import java.lang.Exception;
 
 
-
 public class Exceptions_HW1 {
     public static void main(String[] args) {
         // Реализуйте 3 метода, чтобы в каждом из них получить разные исключения
